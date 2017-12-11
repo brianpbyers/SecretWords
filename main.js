2d92881e211ad2b7c17f97ea41c47b2dc99d65d5
anhonestmouse <anhonestmouse@gmail.com>
2017-12-11 16:44:38 -0700

    starting some stuff. basic styling of css and randomization logic.  put some words in an array to test card functionality

---
+++ main.js
@@ -0,0 +1,46 @@
+
+console.log("this is initializing the racer logic");
+$(document).ready(function(){
+	// dummy words for now.  Will populate with more words and ensure scalability for this purpose.
+	let wordArray = ['Potato','New York', 'Egypt', 'Tree', 'Pen', 'Hat', 'Bubblegum', 'Glasses', 'Fish', 'Boat', 'Water', 'Fish', 'Shark', 'Table', 'Chair', 'Sweatshirt', 'Mug', 'Spoon', 'Laptop', 'Sandals', 'Jacket', 'Pants', 'Bees', 'Ears', 'Eyes', 'Nose', 'Mouth', 'Lips', 'Projector', 'Television', 'Watch', 'Trashcan', 'Rome', 'War', 'Knight', 'Ninja', 'Battle', 'Underwear', 'Australia', 'Kangaroo', 'Snake', 'Whale', 'Narwhal', 'Soda', 'Hair', 'Wig', 'Glasses', 'Pencil', 'Dragon', 'Cow', 'Milk', 'Sheep', 'Goat', 'Cheese', 'Mountain', 'Lake', 'River', 'Window', 'Door', 'Picture', 'Word', 'Frame', 'Girl','Boy', 'Chalkboard', 'Teacher', 'Mt Fuji', 'Skyscraper', 'Koala', 'Apple', 'Orange', 'Banana', 'Steak', 'Chicken', 'Green', 'Blue', 'Black', 'Red', 'Yellow', 'Orange', 'Purple', 'White'];
+	// list of 24 of the 25 game tiles.  The 25th will be determined during the makeGameBoard Process
+	let colorCodes = ['R','R','R','R','R','R','R','R','B','B','B','B','B','B','B','B','T','T','T','T','T','T','T','X'];
+	let p1Win = 0;
+	let p2Win = 0;
+	let randomSeed = Math.floor(Math.random()*100000000);
+	function Card(word, rank, secretColor){
+		this.word = word;
+		this.rank = rank;
+		this.secretColor = secretColor;
+	}
+
+	Card.prototype.flipIt = function(){
+		// do stuff to flip this element.  show secret value.  add animation
+	};
+
+	// making the array that will randomize gameboard and colorcodeArray
+	function makeRandArray(mySeed){
+		let myRandArray = [];
+		let newRand;
+		let iterator = 0;
+		// for(let i = 0; i<50;++i){
+		// 	newRand = Math.sin(mySeed + i) * 10000;
+		// 	newRand = newRand - Math.floor(newRand);
+		// 	myRandArray.push(newRand);
+		// }
+		while(iterator<50){
+			newRand = Math.sin(mySeed + iterator) * 10000;
+			newRand = (newRand - Math.floor(newRand))*wordArray.length;
+			if(!myRandArray.includes(newRand)){
+				myRandArray.push(newRand);
+				iterator++;
+			}
+		}
+		console.log(myRandArray);
+		return(myRandArray);
+	}
+
+	function makeGameBoard(randArray){
+
+	}
+});
