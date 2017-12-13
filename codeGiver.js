8945c81247524acbae34822b4f486d0aa5b67951
anhonestmouse <anhonestmouse@gmail.com>
2017-12-12 20:38:35 -0700

    created code board creation logic

--- codeGiver.js
+++ codeGiver.js
@@ -1,13 +1,38 @@
 console.log("this is initializing codeGiver logic.  main.js should be loaded prior to using this.");
 $(document).ready(function(){
+	let $boardArray = [];
 	$('#clicker').click(function(event){
 		// didn't realize this wasn't returning a number.  Number corrected this.
-		let randSeed = Number($('#seedEntry').val());
+		randSeed = Number($('#seedEntry').val());
+		// storing it now.  Will figure out if necessary later
+		localStorage.setItem("seed",randSeed);
 		randArray = makeRandArray(randSeed);
 		// console.log(randArray);
+		if(randSeed%2 ===1){
+			console.log('red goes first');
+			colorCodes.push('R');
+		} else{
+			console.log('blue goes first');
+			colorCodes.push('B');
+		}
 		mySecretKey = getSecretKey(randArray, colorCodes);
 		firstWord = wordArray[randArray[0]];
 		alert(firstWord + ' should be the first word on the gameBoard.  If not, refresh this page and re-enter the secret key!');
-
+		$('#clicker').text(firstWord + " is correct").css("background-color","green").css("color","white").off();
+		$('#clicker').click(function(){
+				makeCodeBoard(mySecretKey);
+			});
 	});
+
+	function makeCodeBoard(mySecretKey){
+		$('.before').hide();
+		$('.after').show();
+		console.log('making Code Board');
+		for(let i = 0; i<25; ++i){
+			$boardArray[i] = $('<div/>');
+			$($boardArray[i]).html('<div/>').addClass(mySecretKey[i] + ' card');
+			$('#codeBoard').append($($boardArray[i]));
+			console.log(mySecretKey[i]);
+		}
+	}
 });
--- gameBoard.js
+++ gameBoard.js
@@ -1,21 +1,52 @@
 // This is the logic for the gameBoard
 console.log("this is initializing game logic.  main.js should be loaded prior to this.");
 $(document).ready(function(){
-	let p1Win = 0;
-	let p2Win = 0;
+	// cards left until win
+	let redWin = 8;
+	let blueWin = 8;
+	let guesses = 0;
 	// let randomSeed = Math.floor(Math.random()*100000000);
 	let randomSeed = Number(localStorage.getItem("seed"));
 	// console.log('random seed' + randomSeed);
 	let $boardArray = [];
+
+	function whosTurn(){
+		if($('#code').hasClass('redTurn')){
+			return 'Red Team';
+		}else if($('#code').hasClass('blueTurn')){
+			return 'Blue Team';
+		}else {
+			return 'Nobody';
+		}
+	}
+
+	function flip($thisCard){
+		$($thisCard).addClass('flipped');
+		$($thisCard).empty();
+		if($('.R.flipped').length===redWin) {
+			alert("Red Team Wins!");
+		} else if($('.B.flipped').length===blueWin) {
+			alert("Blue Team Wins!");
+		}else if($('.X').hasClass('flipped')) {
+			let whosUp = whosTurn();
+			alert(whosUp + "flipped the Black Card!  They Lose!");
+		}
+
+	}
 
 	function makeGameBoard(randArray){
 		// console.log('making Game Board');
 		if(randomSeed%2 ===1){
 			// console.log('red goes first');
 			colorCodes.push('R');
+			redWin++;
+			$('#code').addClass('redTurn').text("Red will go first");
 		} else{
 			// console.log('blue goes first');
 			colorCodes.push('B');
+			blueWin++
+			$('#code').addClass('blueTurn').text("Blue will go first");
+
 		}
 		let myCodeKey = getSecretKey(randArray, colorCodes);
 		for(let i = 0; i<25; ++i){
@@ -24,11 +55,13 @@ $(document).ready(function(){
 			$('#gameBoard').append($($boardArray[i]));
 			$($boardArray[i]).click(function(){
 				console.log('clicked ' + $($boardArray[i]).text() + ' B?: ' + $($boardArray[i]).hasClass('B') +' R?: ' + $($boardArray[i]).hasClass('R'));
-				flip($boardArray[i]);
-				setTimeout(checkForWin(),1500);
+				if(!$(this).hasClass('flipped')){
+					flip($boardArray[i]);
+				} else {
+					alert("This card has already been flipped.  Please select another one");
+				}
 			});
 		}
 	}
-	console.log(makeRandArray(randomSeed));
 	makeGameBoard(makeRandArray(randomSeed));
 });
